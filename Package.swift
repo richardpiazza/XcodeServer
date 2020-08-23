0bb93d857900718033a19fd9dbc238824449f736
Richard Piazza <richard@richardpiazza.com>
2020-08-23 06:20:45 -0500

    Pinned 'SessionPlus' to the 1.0.0 release.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/richardpiazza/SessionPlus.git", from: "1.0.0-rc.2"),
+        .package(url: "https://github.com/richardpiazza/SessionPlus.git", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/tsolomko/SWCompression", .upToNextMinor(from: "4.5.5")),
         .package(url: "https://github.com/richardpiazza/ProcedureKit.git", .branch("feature/cross-platform")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.1.0")),
