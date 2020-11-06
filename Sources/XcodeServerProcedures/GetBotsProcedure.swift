c2998945b83bec837c99a4b3f82eee06fa60714c
Richard Piazza <richard@richardpiazza.com>
2020-11-06 16:49:20 -0600

    Simplified Procedures (#16)

--- Sources/XcodeServerProcedures/GetBotsProcedure.swift
+++ Sources/XcodeServerProcedures/GetBotsProcedure.swift
@@ -4,11 +4,13 @@ import ProcedureKit
 public class GetBotsProcedure: Procedure, OutputProcedure {
     
     private let source: BotQueryable
+    private let serverId: Server.ID?
     
     public var output: Pending<ProcedureResult<[Bot]>> = .pending
     
-    public init(source: BotQueryable) {
+    public init(source: BotQueryable, serverId: Server.ID? = nil) {
         self.source = source
+        self.serverId = serverId
         super.init()
     }
     
@@ -17,15 +19,30 @@ public class GetBotsProcedure: Procedure, OutputProcedure {
             return
         }
         
-        source.getBots { [weak self] (result) in
-            switch result {
-            case .success(let value):
-                self?.output = .ready(.success(value))
-                self?.finish()
-            case .failure(let error):
-                InternalLog.procedures.error("GetBotsProcedure Failed", error: error)
-                self?.output = .ready(.failure(error))
-                self?.finish(with: error)
+        switch serverId {
+        case .none:
+            source.getBots { [weak self] (result) in
+                switch result {
+                case .success(let value):
+                    self?.output = .ready(.success(value))
+                    self?.finish()
+                case .failure(let error):
+                    InternalLog.procedures.error("GetBotsProcedure Failed", error: error)
+                    self?.output = .ready(.failure(error))
+                    self?.finish(with: error)
+                }
+            }
+        case .some(let id):
+            source.getBots(forServer: id) { [weak self] (result) in
+                switch result {
+                case .success(let value):
+                    self?.output = .ready(.success(value))
+                    self?.finish()
+                case .failure(let error):
+                    InternalLog.procedures.error("GetBotsProcedure Failed", error: error)
+                    self?.output = .ready(.failure(error))
+                    self?.finish(with: error)
+                }
             }
         }
     }
--- Sources/XcodeServerProcedures/SyncBotProcedure.swift
+++ Sources/XcodeServerProcedures/SyncBotProcedure.swift
@@ -1,36 +1,36 @@
 import XcodeServer
 import ProcedureKit
 
-/// Sync - _retrieve & update_ - a single `Server`.
+/// Sync - _retrieve & update_ - a single `Bot`.
 ///
-/// For each `Bot` that available on the server, a `SyncBotProcedure` will be added to the procedure queue.
+/// For each `Integration` that available on the bot, a `SyncIntegrationProcedure` will be added to the procedure queue.
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 @available(swift, introduced: 5.1)
-public class SyncServerProcedure: Procedure, OutputProcedure {
+public class SyncBotProcedure: Procedure, OutputProcedure {
     
     public typealias Source = AnyQueryable
     public typealias Destination = (AnyQueryable & AnyPersistable)
     
     private let source: Source
     private let destination: Destination
-    private let server: Server
-    private let syncBots: Bool
+    private let bot: Bot
+    private let syncIntegrations: Bool
     private let procedureQueue: ProcedureQueue = ProcedureQueue()
     
-    public var output: Pending<ProcedureResult<Server>> = .pending
+    public var output: Pending<ProcedureResult<Bot>> = .pending
     private var getCount: Int = 0
     
-    /// Initializes a `SyncServerProcedure`.
+    /// Initializes a `SyncBotProcedure`.
     ///
-    /// - parameter source: The `AnyQueryable` source for the initial bot query.
+    /// - parameter source: The `AnyQueryable` source for the initial integrations query.
     /// - parameter destination: The `AnyQueryable & AnyPersistable` storage.
-    /// - parameter server: The `Server` that requires syncing.
-    /// - parameter syncBots: Indicates if further bot processing should be executed.
-    public init(source: Source, destination: Destination, server: Server, syncBots: Bool = true) {
+    /// - parameter server: The `Bot` that requires syncing.
+    /// - parameter syncIntegrations: Indicates if further integration processing should be executed.
+    public init(source: Source, destination: Destination, bot: Bot, syncIntegrations: Bool = true) {
         self.source = source
         self.destination = destination
-        self.server = server
-        self.syncBots = syncBots
+        self.bot = bot
+        self.syncIntegrations = syncIntegrations
         super.init()
         
         procedureQueue.delegate = self
@@ -42,49 +42,49 @@ public class SyncServerProcedure: Procedure, OutputProcedure {
             return
         }
         
-        let versions = SyncVersionProcedure(source: source, destination: destination, server: server)
-        let get = GetBotsProcedure(source: source)
-        get.addDependency(versions)
-        let update = UpdateServerBotsProcedure(destination: destination, server: server)
+        let stats = SyncBotStatsProcedure(source: source, destination: destination, bot: bot)
+        let get = GetBotIntegrationsProcedure(source: source, input: bot.id)
+        get.addDependency(stats)
+        let update = UpdateBotIntegrationsProcedure(destination: destination, bot: bot)
         update.injectResult(from: get)
         
-        procedureQueue.addOperations([versions, get, update])
+        procedureQueue.addOperations([stats, get, update])
     }
 }
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 @available(swift, introduced: 5.1)
-extension SyncServerProcedure: ProcedureQueueDelegate {
+extension SyncBotProcedure: ProcedureQueueDelegate {
     public func procedureQueue(_ queue: ProcedureQueue, didAddProcedure procedure: Procedure, context: Any?) {
         InternalLog.procedures.debug("Enqueued Procedure '\(procedure)'")
     }
     
     public func procedureQueue(_ queue: ProcedureQueue, didFinishProcedure procedure: Procedure, with error: Error?) {
         switch procedure {
-        case is UpdateServerBotsProcedure:
-            let getOutput = GetBotsProcedure(source: destination, serverId: server.id)
+        case is UpdateBotIntegrationsProcedure:
+            let getOutput = GetBotIntegrationsProcedure(source: destination, input: bot.id)
             queue.addOperation(getOutput)
-        case is GetBotsProcedure:
+        case is GetBotIntegrationsProcedure:
             switch getCount {
             case 0:
                 getCount += 1
                 break
             case 1:
                 getCount += 1
-                let proc = procedure as! GetBotsProcedure
+                let proc = procedure as! GetBotIntegrationsProcedure
                 switch proc.output.value {
                 case .none:
                     break
                 case .failure(let e):
                     output = .ready(.failure(e))
-                case .success(let bots):
-                    var s = server
-                    s.bots = Set(bots)
-                    output = .ready(.success(s))
+                case .success(let integrations):
+                    var b = bot
+                    b.integrations = Set(integrations)
+                    output = .ready(.success(b))
                     
-                    if syncBots {
-                        bots.forEach({
-                            let sync = SyncBotProcedure(source: source, destination: destination, bot: $0)
+                    if syncIntegrations {
+                        integrations.forEach({
+                            let sync = SyncIntegrationProcedure(source: source, destination: destination, integration: $0)
                             queue.addOperation(sync)
                         })
                     }
